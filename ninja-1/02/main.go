bcb5d54ab22222896a3658a4040dc3b4389d3c01
JuanIrache <yrache@gmail.com>
2020-02-02 02:40:38 +0100

    second exercise

---
+++ ninja-1/02/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+var x int
+var y string
+var z bool
+
+func main() {
+	fmt.Println("Zero values")
+	fmt.Println("x:", x)
+	fmt.Println("y:", y)
+	fmt.Println("z:", z)
+}
