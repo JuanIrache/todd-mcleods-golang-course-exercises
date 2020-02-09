c46a24134a1724454eccdcaebbb1ba0944a38711
JuanIrache <yrache@gmail.com>
2020-02-09 21:50:16 +0100

    use proper type

--- ninja-04/01/main.go
+++ ninja-04/01/main.go
@@ -6,6 +6,6 @@ func main() {
 	arr := [5]int{5, 4, 3, 2, 1}
 	fmt.Printf("Looping through range of array of type %T\n", arr)
 	for _, v := range arr {
-		fmt.Printf(" %v", v)
+		fmt.Printf(" %d", v)
 	}
 }
