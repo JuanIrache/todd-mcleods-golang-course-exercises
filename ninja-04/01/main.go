8f74c9194241d96c79cc956883936f071ca9df29
JuanIrache <yrache@gmail.com>
2020-02-09 21:43:09 +0100

    exercise 1

---
+++ ninja-04/01/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := [5]int{5, 4, 3, 2, 1}
+	fmt.Printf("Looping through range of array of type %T\n", arr)
+	for _, v := range arr {
+		fmt.Printf(" %v", v)
+	}
+}
