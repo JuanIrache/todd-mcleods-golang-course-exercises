f96defe44140be46cc81b187fbd1a0c0876a7984
JuanIrache <yrache@gmail.com>
2020-02-10 00:32:07 +0100

    exercise 9

---
+++ ninja-04/09/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+	mxs := map[string][]string{
+		`bond_james`:      []string{`Shaken, not stirred`, `Martinis`, `Women`},
+		`moneypenny_miss`: []string{`James Bond`, `Literature`, `Computer Science`},
+		`no_dr`:           []string{`Being evil`, `Ice cream`, `Sunsets`},
+	}
+
+	mxs[`gump_forrest`] = []string{`Sweets`, `Running`, `Jane`}
+	for k, xs := range mxs {
+		fmt.Println(k)
+		for _, s := range xs {
+			fmt.Printf("\t%q", s)
+		}
+		fmt.Print("\n")
+	}
+}
