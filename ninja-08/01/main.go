db56eb5797ad3d8b3fc7590c2945cb0b9b2c9401
JuanIrache <yrache@gmail.com>
2020-02-14 00:52:41 +0100

    exercise 1

---
+++ ninja-08/01/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type user struct {
+	First string
+	Age   int
+}
+
+func main() {
+	u1 := user{
+		First: "James",
+		Age:   32,
+	}
+
+	u2 := user{
+		First: "Moneypenny",
+		Age:   27,
+	}
+
+	u3 := user{
+		First: "M",
+		Age:   54,
+	}
+
+	users := []user{u1, u2, u3}
+
+	b, err := json.Marshal(users)
+
+	if err != nil {
+		fmt.Printf("There was an error:\n%q\n", err)
+	}
+	s := string(b)
+
+	fmt.Println(s)
+}
