6d540194f731a5e553d59f1de6af96b6a1acbc44
JuanIrache <yrache@gmail.com>
2020-02-13 02:38:44 +0100

    fibonacci with memo

--- ninja-06/11/main.go
+++ ninja-06/11/main.go
@@ -3,16 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	n := 40
+	n := 100
 	fmt.Printf("Fibonacci %d is %d\n", n, fibonacci(n))
 }
 
+var memo = map[int]int{
+	0: 0,
+	1: 1,
+}
+
 func fibonacci(n int) int {
-	if n == 0 {
-		return 0
+	if _, ok := memo[n]; !ok {
+		memo[n] = fibonacci(n-1) + fibonacci(n-2)
 	}
-	if n == 1 || n == 2 {
-		return 1
-	}
-	return fibonacci(n-1) + fibonacci(n-2)
+	return memo[n]
 }
