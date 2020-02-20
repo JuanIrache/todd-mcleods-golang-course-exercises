c903a94e21542e6d220939837f2d93908bfa9545
JuanIrache <yrache@gmail.com>
2020-02-20 13:49:09 +0100

    add and explain assertion

--- ninja-11/03/main.go
+++ ninja-11/03/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 type customErr struct {
 	msg string
@@ -19,4 +22,12 @@ func foo(e error) {
 	fmt.Println("Foo ran")
 	fmt.Println(e)
 	fmt.Printf("%T\n", e)
+	fmt.Println("e can use assertion to use as customErr and retrieve msg: ", e.(customErr).msg)
+	y := errors.New("New error")
+	if msg, ok := y.(customErr); ok {
+		fmt.Println("new error y can use assertion to use as customErr and retrieve msg: ", msg)
+	} else {
+		fmt.Println("new error y can not use assertion to use as customErr and retrieve msg: ")
+
+	}
 }
